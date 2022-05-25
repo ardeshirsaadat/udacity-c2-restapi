56e4b1d6dd91cda28622e97aa16d1fe2483e48a9
Ardeshir Saadat <ardeshirsaadat@yahoo.com>
2022-05-25 11:36:20 -0400

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,23 @@
+
+export const config = {
+  "dev": {
+    "username": process.env.USERNAME_POSTGRES,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
